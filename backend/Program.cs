bfdfc55d8cce7181136cf255322902f2754771d9
Vahab Lotfyyekta <vahab.lotfyyekta@it-minds.dk>
2026-09-16 11:36:58 +0200

    Configure CORS for frontend

--- backend/Program.cs
+++ backend/Program.cs
@@ -50,11 +50,25 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 
             ValidateLifetime = true
         };
-    });
+});
+
+builder.Services.AddCors(options =>
+{
+        options.AddPolicy("Frontend", policy =>
+        {
+            policy
+                .WithOrigins("http://localhost:5173")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+            });
+});
 
 
 builder.Services.AddAuthorization();
+
 var app = builder.Build();
+
+app.UseCors("Frontend");
 
 app.UseAuthentication();
 app.UseAuthorization();
